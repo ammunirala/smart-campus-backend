bf5d1ed8f54103bfc06caaab76886bd16b7ae6b4
Amresh Nirala <138843165+ammunirala@users.noreply.github.com>
2026-07-29 11:42:18 +0530

    Make API URL deployment ready

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,19 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8081/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8081/api",
+  headers: { "Content-Type": "application/json" },
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
+  if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
 
